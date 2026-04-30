84df7b2e7ccb51d0cffdd5ccbd2a1a7e59a3b98f
Felpss14 <felipenestor1407@gmail.com>
2026-04-30 11:25:00 -0300

    adição de imagens

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
-  function atualizarContador() {
-    const agora = new Date();
-    
-    // Data alvo: 28 de junho de 2026 às 16:00
-    const destino = new Date("2026-06-28T16:00:00");
+const carrossel = document.querySelector('.carrossel');
 
-    const diferenca = destino - agora;
+// DUPLICA os cards (loop infinito)
+carrossel.innerHTML += carrossel.innerHTML;
 
-    if (diferenca <= 0) {
-      document.getElementById("tempo").innerHTML = "Chegou o grande dia!";
-      return;
+let scrollAmount = 0;
+const velocidade = 0.8;
+let pausado = false;
+
+function animar() {
+  if (!pausado) {
+    scrollAmount += velocidade;
+
+    // quando chega na metade (fim da primeira lista)
+    if (scrollAmount >= carrossel.scrollWidth / 2) {
+      scrollAmount = 0;
     }
 
-    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
-    const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
-    const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
-    const segundos = Math.floor((diferenca / 1000) % 60);
-
-    document.getElementById("tempo").innerHTML =
-      dias + " dias, " +
-      horas + " horas, " +
-      minutos + " minutos, " +
-      segundos + " segundos";
+    carrossel.scrollLeft = scrollAmount;
   }
 
-  setInterval(atualizarContador, 1000);
+  requestAnimationFrame(animar);
+}
+
+animar();
+
+// pausa no toque (mobile)
+carrossel.addEventListener("touchstart", () => {
+  pausado = true;
+});
+
+carrossel.addEventListener("touchend", () => {
+  pausado = false;
+});
+
+//Contador
+function atualizarContador() {
+  const agora = new Date();
+  const destino = new Date("2026-06-28T16:00:00");
+
+  const diferenca = destino - agora;
+
+  if (diferenca <= 0) {
+    document.getElementById("tempo").innerHTML = "Chegou o grande dia!";
+    return;
+  }
+
+  const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
+  const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
+  const segundos = Math.floor((diferenca / 1000) % 60);
+
+  document.getElementById("tempo").innerHTML =
+    dias + " dias, " +
+    horas + " horas, " +
+    minutos + " minutos, " +
+    segundos + " segundos";
+}
+
+// inicia imediatamente + atualiza a cada 1s
+atualizarContador();
+setInterval(atualizarContador, 1000);
